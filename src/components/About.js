70905bf9d5a894de942493067e5c49a553c67225
waltersimekha <waltersimekha@gmail.com>
2023-04-18 14:52:53 +0300

    completed assignment

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,9 @@ function About() {
     <div id="about">
       <h2>About Me</h2>
       <p>I Made This</p>
-      <img src={image} alt="I made this" />
+      <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
     </div>
   );
 }
+
+export default About;
--- src/components/Home.js
+++ src/components/Home.js
@@ -4,8 +4,9 @@ function Home() {
   return (
     <div id="home">
       <h1>
-        {username} is a Web Developer from {city}
+        Liza is a Web Developer from New York
       </h1>
     </div>
   );
 }
+export default Home;
